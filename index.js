53a4a891acecbc644c877c98252ee6d8f377a952
Martin Armenski <armenskim7@gmail.com>
2026-01-24 15:05:35 -0500

    Finished project

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ function rollDice(){
     const values = [];
     const images = [];
     
+
+    for (let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="images/dice${value}.png">`)
+    }
+    diceResult.textContent=`dice: ${values.join(', ')}`;
+    diceImages.innerHTML = images.join('');
 }
